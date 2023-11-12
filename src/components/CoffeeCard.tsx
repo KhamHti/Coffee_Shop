9559733061adab3bf7c0454e385e7bdab4268052
Khun Kyaw Kham Hti <khamhti@agga.io>
2023-11-13 00:37:32 +0630

    components created

---
+++ src/components/CoffeeCard.tsx
@@ -0,0 +1,157 @@
+import {
+  Dimensions,
+  ImageBackground,
+  ImageProps,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import BGIcon from './BGIcon';
+
+const CARD_WIDTH = Dimensions.get('window').width * 0.32;
+
+interface CoffeeCardProps {
+  id: string;
+  index: number;
+  type: string;
+  rosted: string;
+  imagelink_square: ImageProps;
+  name: string;
+  special_ingredient: string;
+  average_rating: number;
+  price: any;
+  buttonPressHandler: any;
+}
+
+const CoffeeCard: React.FC<CoffeeCardProps> = ({
+  id,
+  index,
+  type,
+  rosted,
+  imagelink_square,
+  name,
+  special_ingredient,
+  average_rating,
+  price,
+  buttonPressHandler,
+}) => {
+  return (
+    <LinearGradient
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 1}}
+      style={styles.CardLinearGradientContainer}
+      colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}>
+      <ImageBackground
+        source={imagelink_square}
+        style={styles.CardImageBG}
+        resizeMode="cover">
+        <View style={styles.CardRatingContainer}>
+          <Icon
+            name="star"
+            size={FONTSIZE.size_20}
+            color={COLORS.primaryOrangeHex}
+          />
+          <Text style={styles.CardRatingText}>{average_rating}</Text>
+        </View>
+      </ImageBackground>
+      <Text style={styles.CardTitle}>{name}</Text>
+      <Text style={styles.CardSubTitle}>{special_ingredient}</Text>
+
+      <View style={styles.CardFooterRow}>
+        <View style={styles.CardPriceCurrency}>
+          <Icon
+            name="currency-usd"
+            size={SPACING.space_20}
+            color={COLORS.primaryOrangeHex}
+          />
+          <Text style={styles.CardPrice}>{price.price}</Text>
+        </View>
+        <TouchableOpacity onPress={() => {}}>
+          {/* <Icon
+            name="plus"
+            size={SPACING.space_20}
+            color={COLORS.primaryOrangeHex}
+          /> */}
+          <BGIcon
+            name={'plus'}
+            size={FONTSIZE.size_16}
+            color={COLORS.primaryWhiteHex}
+            BGColor={COLORS.primaryOrangeHex}
+          />
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default CoffeeCard;
+
+const styles = StyleSheet.create({
+  CardLinearGradientContainer: {
+    padding: SPACING.space_15,
+    borderRadius: BORDERRADIUS.radius_25,
+  },
+  CardImageBG: {
+    width: CARD_WIDTH,
+    height: CARD_WIDTH,
+    borderRadius: BORDERRADIUS.radius_20,
+    marginBottom: SPACING.space_15,
+    overflow: 'hidden',
+  },
+  CardRatingContainer: {
+    flexDirection: 'row',
+    backgroundColor: COLORS.primaryBlackRGBA,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: SPACING.space_10,
+    paddingHorizontal: SPACING.space_15,
+    position: 'absolute',
+    borderBottomLeftRadius: BORDERRADIUS.radius_20,
+    borderTopRightRadius: BORDERRADIUS.radius_20,
+    top: 0,
+    right: 0,
+  },
+  CardRatingText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    color: COLORS.primaryWhiteHex,
+    fontSize: FONTSIZE.size_16,
+    lineHeight: 22,
+  },
+  CardFooterRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: SPACING.space_15,
+  },
+  CardTitle: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    color: COLORS.primaryWhiteHex,
+    fontSize: FONTSIZE.size_16,
+  },
+  CardSubTitle: {
+    fontFamily: FONTFAMILY.poppins_light,
+    color: COLORS.primaryWhiteHex,
+    fontSize: FONTSIZE.size_12,
+  },
+  CardPrice: {
+    color: COLORS.primaryWhiteHex,
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_20,
+  },
+  CardPriceCurrency: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    color: COLORS.primaryOrangeHex,
+  },
+});
